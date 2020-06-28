c4145bddc6761301ec3472a92b2401ae95531b06
nidhishtk <nidhisht6@gmail.com>
2020-06-28 08:41:53 -0700

    my first program in python

---
+++ FirstPythonCode.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 28 07:01:43 2020
+
+@author: Nidhish T.K
+"""
+
+#please dont remove this line
+"""
+jad
+djksje
+djnsjd
+"""
+
+print("hello",end=(","))#
+print("i am nidhish")
+
+print("hello","i am nidhish")
+
+print("hello \n  i am nidhish \t 1")
